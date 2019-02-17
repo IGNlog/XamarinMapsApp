0964c317aaf22863aa78b1f489294faeb7976f6d
Ildar Gibadullin <iglog5555@hotmail.com>
2019-02-17 19:33:50 +0300

    does not start on android

--- XamarinMapsApp/XamarinMapsApp/Views/FavoritePlacePage.xaml.cs
+++ XamarinMapsApp/XamarinMapsApp/Views/FavoritePlacePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ namespace XamarinMapsApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FavoritePlacePage : ContentPage
     {
+        //public ImageSource ImageSourcePhoto;
+
         public FavoritePlacePage()
         {
             InitializeComponent();
@@ -38,6 +42,36 @@ namespace XamarinMapsApp.Views
         private void Cancel(object sender, EventArgs e)
         {
             this.Navigation.PopAsync();
+        }
+
+        private async void takePhotoBtn_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Sample",
+                    Name = $"{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                img.Source = ImageSource.FromFile(file.Path);
+                //ImageSourcePhoto = ImageSource.FromFile(file.Path);
+            }
+        }
+
+        private async void getPhotoBtn_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (CrossMedia.Current.IsPickPhotoSupported)
+            {
+                MediaFile photo = await CrossMedia.Current.PickPhotoAsync();
+                img.Source = ImageSource.FromFile(photo.Path);
+            }
         }
     }
 }
